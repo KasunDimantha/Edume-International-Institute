5f6ac1c57a42e321e7410a029674cac228a248bf
KasunDimantha <kasundimantha97@gmail.com>
2024-02-21 08:19:19 +0530

    Initialize home page and components

--- EdumeLMS/src/App.jsx
+++ EdumeLMS/src/App.jsx
@@ -1,10 +1,17 @@
 import './App.css'
+import { BrowserRouter as Router,Route,Routes } from 'react-router-dom'
+import Home_Page from './pages/Home_Page'
+
 
 function App() {
 
   return (
     <>
-      
+      <Router>
+        <Routes>
+          <Route exact path="/"  element={<Home_Page/>}/>
+        </Routes>
+      </Router>
     </>
   )
 }
---
+++ EdumeLMS/src/pages/Home_Page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Home_Page() {
+  return (
+    <div>
+      <h1>kasun</h1>
+    </div>
+  )
+}
+
+export default Home_Page
